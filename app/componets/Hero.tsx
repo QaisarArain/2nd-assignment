3612610388438108be3362f1c36aa2a72ca5d20b
QaisarArain <160647472+QaisarArain@users.noreply.github.com>
2024-10-27 23:16:02 +0500

    Update Hero.tsx

--- app/componets/Hero.tsx
+++ app/componets/Hero.tsx
@@ -2,9 +2,12 @@ const Hero = () => {
     return (
       <section className="bg-gradient-to-r from-slate-400 via-slate-500 to-slate-600 py-20 flex justify-center items-center">
         <div className="bg-white rounded-xl shadow-lg p-12 max-w-3xl text-center transition-transform transform hover:scale-105 hover:rotate-2">
-          <h2 className="text-4xl font-bold text-stone-900">"Innovating Tomorrow, Today</h2>
+          <h2 className="text-4xl font-bold text-stone-900">"Qaisar Arain Portfolio</h2>
           <p className="mt-6 text-stone-700 text-lg leading-relaxed">
-          At NexaVision Labs, we push the boundaries of innovation to empower businesses and individuals with cutting-edge solutions. Let us help you create the future you've always envisioned.
+          QaisarArain
+With a deep-rooted love for creating intuitive, user-friendly websites, I bring technical expertise and creative design together to deliver powerful web solutions. I specialize in building responsive, high-performance websites that not only look great but also provide seamless user experiences.
+
+Let's connect and turn ideas into impactful web experiences!
           </p>
         </div>
       </section>
