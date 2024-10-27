588b1e83767e2dfc0538b5e57bc6019e33a6c3d8
QaisarArain <160647472+QaisarArain@users.noreply.github.com>
2024-10-27 23:17:49 +0500

    Update page.tsx

--- about/page.tsx
+++ about/page.tsx
@@ -4,9 +4,13 @@ const AboutPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-indigo-200">
       <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-3xl font-bold text-stone-800 mb-4">Innovating Tomorrow, Today</h2>
+        <h2 className="text-3xl font-bold text-stone-800 mb-4">About Me</h2>
         <p className="text-stone-600 text-lg leading-relaxed">
-        At NexaVision Labs, we push the boundaries of innovation to empower businesses and individuals with cutting-edge solutions. Let us help you create the future you've always envisioned.
+       I’m Qaisar Arain, a dedicated web developer with a strong focus on creating efficient and engaging online experiences. With expertise in both front-end and back-end technologies, I craft responsive, visually appealing, and user-friendly websites that align with the latest industry trends and best practices.
+
+Driven by curiosity and innovation, I love exploring new tools and techniques to continuously improve my skills and deliver top-notch results for clients. Whether it's developing a simple website or a complex web application, I strive for excellence in every project, ensuring a seamless and enjoyable experience for end-users.
+
+Let’s connect and build something remarkable together!
         </p>
       </div>
     </div>
